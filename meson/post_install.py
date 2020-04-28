df78584e25eb73667b3310a224771e48b098d07e
Olivier Duchateau <duchateau.olivier@gmail.com>
2020-04-28 18:30:11 +0200

    Fix symlink creation

--- meson/post_install.py
+++ meson/post_install.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 
+import os
 import pathlib
 import sys
 
+def remove_prefix(path, prefix):
+	p = pathlib.Path(path)
+	return '{0}'.format(p.relative_to(prefix))
+
+def new_prefix(basename, prefix):
+	'''Return pathlib.Path object.'''
+	p = pathlib.Path(prefix)
+	return p.joinpath(basename)
+
 # Variables from meson.add_install_script() function
 xml_file = sys.argv[1]
-src_dir = sys.argv[2]
-dst_dir = sys.argv[3]
+dst_dir = remove_prefix(sys.argv[3], os.environ['MESON_INSTALL_PREFIX'])
 
-# We work with pathlib.Path object
-p = pathlib.Path(dst_dir)
+# Find relative path between sys.argv[2] and sys.argv[3]
+rel_dir = os.path.relpath(sys.argv[2], sys.argv[3])
+
+p = new_prefix(dst_dir, os.environ['MESON_INSTALL_DESTDIR_PREFIX'])
 if not p.exists():
     p.mkdir(parents=True)
 dst = p.joinpath(xml_file)
 
 # We want a 'string' object
-src = '{0}'.format(pathlib.Path(src_dir).joinpath(xml_file))
+src = '{0}'.format(pathlib.Path(rel_dir).joinpath(xml_file))
 
 # Create symlink
 # https://docs.python.org/3/library/pathlib.html#pathlib.Path.symlink_to
